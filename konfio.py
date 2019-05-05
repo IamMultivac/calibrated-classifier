3a4ceff08ddf93771afe1c36a64af1dcde238b42
juliolunamoreno <jlqmoreno@gmail.com>
2019-05-05 14:31:09 -0500

    deleted librries for colab

--- konfio.py
+++ konfio.py
@@ -13,7 +13,7 @@ Original file is located at
 # Necessary libraries for data analisys
 """
 
-from google.colab import files
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
